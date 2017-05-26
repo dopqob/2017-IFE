0b6cceb4630f3b1387cf45c41ee9a92bb8b813a7
Bilon <30884413@qq.com>
2017-05-26 16:16:51 +0800

    2017.5.26 Company 16:16

---
+++ IFEyaoyao/script/task04.js
@@ -0,0 +1,105 @@
+var trs = document.getElementsByTagName("tr"),
+	command = document.getElementById("command");
+	inputBox = command.children[0],		//输入框
+	execute = command.children[1],		//执行按钮
+	btns = document.getElementById("btns"),
+	go = btns.children[0],				//前进按钮
+	turnLeft = btns.children[1],		//左转按钮
+	turnRight = btns.children[2],		//右转按钮
+	reversal = btns.children[3];		//翻转按钮
+var num = 0;
+
+// 左转，即顺时针旋转270°
+turnLeft.onclick = function() {
+	num = num + 270;
+	box.style.transform = "rotate(" + num + "deg)";
+}
+
+// 右转，即顺时针旋转90°
+turnRight.onclick = function() {
+	num = num + 90;
+		box.style.transform = "rotate(" + num + "deg)";
+}
+
+// 翻转，即顺时针旋转180°
+reversal.onclick = function() {
+	num = num + 180;
+		box.style.transform = "rotate(" + num + "deg)";
+}
+
+/**
+ * 先获取方块当前tr和td的index，即x,y
+ * 然后通过num的值来判断方块的朝向
+ * 最后根据朝向改变x y的值及innerHTML来达到前进效果
+ */
+function move() {
+	var	box = document.getElementById("box"),
+		parentBox = box.parentNode,
+		content = parentBox.innerHTML;
+	var x,y;
+	num = num % 360;
+	for (var i = 0; i < trs.length; i++) {
+		for (var j = 0; j < trs[i].children.length; j++) {
+			if(trs[i].children[j] == parentBox) {
+				x = i;
+				y = j;
+				break;
+			}
+		}
+	}
+	if(num == 0) {
+		if(x - 1 > 0) {
+			x = x - 1;
+			trs[x].children[y].innerHTML = content;
+			parentBox.innerHTML = "";
+		}
+	} else if(num == 90) {
+		if(y + 1 < trs.length) {
+			y = y + 1;
+			trs[x].children[y].innerHTML = content;
+			parentBox.innerHTML = "";
+		}
+	} else if(num == 180){
+		if(x + 1 < trs.length) {
+			x = x + 1;
+			trs[x].children[y].innerHTML = content;
+			parentBox.innerHTML = "";
+		}
+	} else {
+		if(y - 1 > 0) {
+			y = y - 1;
+			trs[x].children[y].innerHTML = content;
+			parentBox.innerHTML = "";
+		}
+	}
+}
+// 前进按钮绑定事件
+go.onclick = function() {
+	move();
+}
+
+// 执行按钮绑定事件
+execute.onclick = function() {
+	var val = inputBox.value;
+	if(!val) {
+		alert("请输入命令");
+		inputBox.focus();
+	}
+	switch (val) {
+		case "TUN LEF":
+			num = num + 270;
+			box.style.transform = "rotate(" + num + "deg)";
+			break;
+		case "TUN RIG":
+			num = num + 90;
+			box.style.transform = "rotate(" + num + "deg)";
+			break;
+		case "TUN BAC":
+			num = num + 180;
+			box.style.transform = "rotate(" + num + "deg)";
+			break;
+		case "GO":
+			move();
+			break;
+	}
+}
